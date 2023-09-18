22461b16a80c39afc0aa1d76cb989d65dcdf7363
Karl Jahnel <karl-jahnel@web.de>
2023-09-18 20:03:50 +0200

    finally full video of farming simulator

--- screencap.py
+++ screencap.py
@@ -1,45 +1,53 @@
 import win32gui
 import win32ui
-from ctypes import windll
+from ctypes import windll, wintypes
 from PIL import Image
+import numpy as np
+import cv2
 
+windll.user32.SetProcessDPIAware()
 hwnd = win32gui.FindWindow(None, 'Farming Simulator 22')
-
-# Change the line below depending on whether you want the whole window
-# or just the client area. 
-#left, top, right, bot = win32gui.GetClientRect(hwnd)
 left, top, right, bot = win32gui.GetWindowRect(hwnd)
 w = right - left
 h = bot - top
 
-hwndDC = win32gui.GetWindowDC(hwnd)
-mfcDC  = win32ui.CreateDCFromHandle(hwndDC)
-saveDC = mfcDC.CreateCompatibleDC()
+# Definieren Sie den codec und erstellen Sie ein VideoWriter-Objekt
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
-saveBitMap = win32ui.CreateBitmap()
-saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (w, h))
 
-saveDC.SelectObject(saveBitMap)
+try:
+    while True:
+        left, top, right, bot = win32gui.GetWindowRect(hwnd)
+        w = right - left
+        h = bot - top
 
-# Change the line below depending on whether you want the whole window
-# or just the client area. 
-#result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 1)
-result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 3)
-print(result)
+        hwndDC = win32gui.GetWindowDC(hwnd)
+        mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+        saveDC = mfcDC.CreateCompatibleDC()
+        saveBitMap = win32ui.CreateBitmap()
+        saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+        saveDC.SelectObject(saveBitMap)
 
-bmpinfo = saveBitMap.GetInfo()
-bmpstr = saveBitMap.GetBitmapBits(True)
+        windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 2)
+        bmpinfo = saveBitMap.GetInfo()
+        bmpstr = saveBitMap.GetBitmapBits(True)
 
-im = Image.frombuffer(
-    'RGB',
-    (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
-    bmpstr, 'raw', 'BGRX', 0, 1)
+        im = Image.frombuffer('RGB', (bmpinfo['bmWidth'], bmpinfo['bmHeight']), bmpstr, 'raw', 'BGRX', 0, 1)
+        frame = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
 
-win32gui.DeleteObject(saveBitMap.GetHandle())
-saveDC.DeleteDC()
-mfcDC.DeleteDC()
-win32gui.ReleaseDC(hwnd, hwndDC)
+        out.write(frame)
+        
+        # Ressourcen für diesen Frame freigeben
+        win32gui.DeleteObject(saveBitMap.GetHandle())
+        saveDC.DeleteDC()
+        mfcDC.DeleteDC()
+        win32gui.ReleaseDC(hwnd, hwndDC)
 
-if result == 1:
-    #PrintWindow Succeeded
-    im.save("test.png")
+        # Zeigt den Frame für eine bessere Kontrolle
+        cv2.imshow('Recording', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+finally:
+    out.release()
+    cv2.destroyAllWindows()
